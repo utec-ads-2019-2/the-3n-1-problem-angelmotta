294cb4ef48780e703ed3e74072862ed241800df8
Angel Motta <angelmotta@gmail.com>
2019-08-22 17:32:47 -0500

    Recursive code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 
-using namespace std;
+int cycle_length;
 
-int main(int argc, char *argv[]) {
+long long int algo(long long int n){
+	if(n == 1){
+		return cycle_length;
+	}
+	else{
+		if(n%2 != 0){
+			cycle_length++;
+			return algo(3 * n + 1);
+		}
+		else{
+			cycle_length++;
+			return algo(n/2);
+		}
+	}
+}
+
+int main() {
+	int i, j, temp, max = 0, result = 0, reverse=0;
+	std::cin >> i >> j;		// i=20 j=10
+	//std::cout << algo(n) << std::endl;
+	if(i > j){
+		temp = i;
+		i = j;	// i = 10;
+		j = temp;	// j = 20;
+		reverse = 1;
+
+	}
+	for(int n = i; n <= j; n++){
+		cycle_length = 1;
+		//std::cout << algo(n) << std::endl;
+		result = algo(n);
+		if(result > max){
+			max = result;
+		}
+	}
+	if(reverse){
+		temp = j;
+		j = i;
+		i = temp;
+	}
+	std::cout << i << " " << j << " " << max << std::endl;
     return 0;
 }
